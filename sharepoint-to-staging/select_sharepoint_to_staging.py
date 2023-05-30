e3ba418f5cad3e6e2461c6593cb49ad44a67acaa
tOWERoFjOY <dandrus1617@gmail.com>
2023-05-30 14:32:31 -0400

    Update select_sharepoint_to_staging.py

--- sharepoint-to-staging/select_sharepoint_to_staging.py
+++ sharepoint-to-staging/select_sharepoint_to_staging.py
@@ -19,6 +19,7 @@ wh_un = os.getenv("wh_user")
 wh_pw = os.getenv("wh_pass")
 schema = os.getenv("schema", 'Staging')
 columns = os.getenv("columns")
+v_force = os.getenv("varchar_force")
 
 sheet = os.getenv("sheet", '')
 skip = int(os.getenv('skip', 0))
@@ -73,6 +74,21 @@ headers = {'authorization': bearer}
 
 columns_select = json.loads(columns)
 column_list = [sub['COLUMN_NAME'] for sub in columns_select]
+# columns_dtype = [sub['DATA_TYPE'] for sub in columns_select]
+# columns_dtype = list(map(str.upper, columns_dtype))
+# column_converter = dict(zip(column_list, columns_dtype))
+
+if not v_force:
+    v_force = None
+else:
+    v_force = v_force.split()
+
+if v_force is not None:
+    v_force_list = {}
+    for i in v_force:
+        v_force_list[i] = sa.types.VARCHAR
+else:
+    v_force_list = None
 
 if drive_type == 'drives':
     url = "https://graph.microsoft.com/v1.0/drives/{}/items/{}/content".format(drive, file_id)
@@ -101,11 +117,15 @@ else:
     if skip == 0:
         df = pd.read_excel(filename, sheet_name=sheet, usecols=column_list)
     else:
-        df = pd.read_excel(filename, sheet_name=sheet, skiprows=range(0, skip), usecols=column_list)
+        df = pd.read_excel(filename, sheet_name=sheet, skiprows=range(0, skip),
+                           usecols=column_list)
 
 if filetype != 'x':
     df = df.loc[:, ~df.columns.str.startswith('Unnamed')]
 
 print(df.head())
+# test_types = dict(zip(
+#     df.columns.tolist(),
+#     (types.VARCHAR(length=20), types.Integer(), types.Float()) ))
 
-df.to_sql(name=table_name, schema=schema, con=engine, if_exists='replace', index=False)
+df.to_sql(name=table_name, schema=schema, con=engine, if_exists='replace', index=False, dtype=v_force_list)
